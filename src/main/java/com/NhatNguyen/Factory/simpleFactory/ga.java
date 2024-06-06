64af70e63b0b296d874a234cbebade7832c48767
Nguyen Dinh Nhat <102273811+dinhnhatnguyen@users.noreply.github.com>
2024-06-06 08:40:41 +0700

    Update ga.java

--- src/main/java/com/NhatNguyen/Factory/simpleFactory/ga.java
+++ src/main/java/com/NhatNguyen/Factory/simpleFactory/ga.java
@@ -3,6 +3,6 @@ package com.NhatNguyen.Factory.simpleFactory;
 public class ga implements DongVat{
     @Override
     public void keu() {
-        System.out.println("đm con gà lày");
+        System.out.println("con gà lày");
     }
 }
